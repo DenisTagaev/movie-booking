cb3952a9de371a3c43eccedaa301b78c05f6562a
Denis Tagaev <sivean.27@gmail.com>
2025-05-29 02:36:21 -0400

    Introduced basic error handling and CORS package for communication with frontend

--- backend/booking_backend/views.py
+++ backend/booking_backend/views.py
@@ -1,8 +1,15 @@
 from django.http import JsonResponse
 from .utils.omdb_fetcher import fetch_random_omdb_movie
-
+from django.views.decorators.http import require_GET
+@require_GET
 def omdb_movies_view(request):
     movie=fetch_random_omdb_movie()
     if movie:
         return JsonResponse(movie)
     return JsonResponse({"error": "Movie not found"}, status=404)
+
+def custom_404_view( request, exception ):
+    return JsonResponse({'detail': 'Not Found'}, status=404)
+
+def health_check(request):
+    return JsonResponse({'status': 'ok'})
